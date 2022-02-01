64f07cd97d7e446e8578d418a2707bfc623aa9ea
Chris H <charmon@dlas.virginia.gov>
2022-02-01 10:33:04 -0500

    no need for init ctor on stateratearea

--- stateRateArea.rb
+++ stateRateArea.rb
@@ -1,10 +1,3 @@
 class StateRateArea
-
-  def initialize(zipcode)
-    state = zipcode.state
-	rate_area = zipcode.rate_area
-  end
-
-  attr_accessor :state, :rate_area
-
+  attr_accessor :state, :rate_area 
 end
