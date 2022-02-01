c5509f23a645447921c9c38d7dfbc4778faf7fcd
Chris H <charmon@dlas.virginia.gov>
2022-02-01 10:33:19 -0500

    build out SLCSP.Get

--- sLCSP.rb
+++ sLCSP.rb
@@ -1,5 +1,13 @@
 class SLCSP
-  def SLCSP.Get()
-    return nil
+  def SLCSP.Get(plans)
+    if plans == nil || plans.count == 0
+      return nil
+    end
+    silverPlans = plans.select { |p| p.metal_level == "Silver" }
+    sortedSilverPlans = silverPlans.sort_by { |p| p.rate }
+    if sortedSilverPlans.count <= 1
+      return nil
+    end
+    return sortedSilverPlans[1].rate
   end
 end
