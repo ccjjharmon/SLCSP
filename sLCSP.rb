945f2e7517f4382ff751e2ccd78442f979800f2b
Chris Harmon <ccjjharmon@gmail.com>
2022-02-01 20:34:43 -0500

    nuanced SLCSP behavior for distinct rates

--- sLCSP.rb
+++ sLCSP.rb
@@ -3,11 +3,13 @@ class SLCSP
     if plans == nil || plans.count == 0
       return nil
     end
-    silverPlans = plans.select { |p| p.metal_level == "Silver" }
-    sortedSilverPlans = silverPlans.sort_by { |p| p.rate }
-    if sortedSilverPlans.count <= 1
+    silverPlans = plans.select { |p| p.metal_level == "Silver" } # filter plans to just Silver
+	sortedSilverPlans = silverPlans.sort_by { |p| p.rate } # sort by rate
+	sp1 = sortedSilverPlans.map { |p| p.rate } # extract just rates
+	sp2 = sp1.uniq # remove duplicates    	 
+    if sp2.count <= 1
       return nil
     end
-    return sortedSilverPlans[1].rate
+    return sp2[1]
   end
 end
